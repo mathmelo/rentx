ce5c4154542cf2ed0ae7373450ab490c95a62e77
Matheus Melo <dev.matheus.melo@gmail.com>
2022-12-09 22:02:42 -0300

    feat: set category id with uuid lib

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,20 @@
 import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({ name, description });
+  const category = {
+    name,
+    description,
+    id: uuid(),
+  };
+
+  categories.push(category);
 
   return response.status(201).send();
 });
