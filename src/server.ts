99d73cfc7ea17c40cfd086c9653f48a28ff9240b
Matheus Melo <dev.matheus.melo@gmail.com>
2022-12-09 21:33:32 -0300

    feat: debbug config added

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'hello world' });
-});
+app.use(express.json());
 
-app.listen(3333);
+app.listen(3333, () => console.log('> Server is running on port 3333'));
