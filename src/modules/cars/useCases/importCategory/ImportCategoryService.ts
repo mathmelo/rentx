ecfd77fd4802485d619bf9ed36cf4fdebf855cf1
Matheus Melo <dev.matheus.melo@gmail.com>
2022-12-15 23:54:30 -0300

    feat: reading csv file with streams

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryService.ts
@@ -0,0 +1,46 @@
+import { parse } from 'csv-parse';
+import fs from 'fs';
+
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+class ImportCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+
+      const parseFile = parse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on('data', (line) => {
+          const [name, description] = line;
+
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on('end', () => {
+          resolve(categories);
+        })
+        .on('error', (err) => reject(err));
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    console.log(categories);
+  }
+}
+
+export { ImportCategoryService };
