06b17b660147100b836344fafc61da570666f315
Matheus Melo <dev.matheus.melo@gmail.com>
2023-08-22 12:46:29 -0300

    feat: specification_cars migration added

---
+++ src/shared/infra/typeorm/migrations/1692716115172-CreateSpecificationsCars.ts
@@ -0,0 +1,74 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateSpecificationsCars1692716115172
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'specifications_cars',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'car_id',
+            type: 'uuid',
+          },
+          {
+            name: 'specification_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'specifications_cars',
+      new TableForeignKey({
+        name: 'FKSpecificationCar',
+        columnNames: ['specification_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'specifications',
+        onDelete: 'SET NULL',
+        onUpdate: 'SET NULL',
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'specifications_cars',
+      new TableForeignKey({
+        name: 'FKCarSpecification',
+        columnNames: ['car_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'cars',
+        onDelete: 'SET NULL',
+        onUpdate: 'SET NULL',
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'specifications_cars',
+      'FKCarSpecification'
+    );
+    await queryRunner.dropForeignKey(
+      'specifications_cars',
+      'FKSpecificationCar'
+    );
+    await queryRunner.dropTable('specifications_cars');
+  }
+}
